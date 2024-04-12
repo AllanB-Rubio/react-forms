74e350e1cdc1e29e0b2683d502a8c212b41788cc
Allan Rubio <allanrubio@MacBook-Pro.local>
2024-04-11 17:31:15 -0700

    fetching API / result succcessful

--- React-Forms/src/components/SignUpForm.jsx
+++ React-Forms/src/components/SignUpForm.jsx
@@ -9,15 +9,24 @@ export default function SignUpForm() {
     e.preventDefault();
 
     try {
+        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+                method: "POST",
+                body: JSON.stringify({username, password})
+                },
+        );
+        const result = await response.json(); 
+        console.log(result);
+
     } catch (error) {
         setError(error.message)
     }
+    //set" "
   }
 
   return (
     <>
       <h2>Sign Up </h2>
-      {error && <p>{error}</p>}
+      {error && <p>{error}</p>} {/* '&&' evaluates to 'true' if both are true, it evaluates to false. The double && is being used for conditional rendering: it renders the <p> element only if error is truthy*/}
 
       <form onSubmit={handleSubmit}>
 
