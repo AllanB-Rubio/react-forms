d6180b908c9c5ea4f238faf8520605074b230e3a
Allan Rubio <allanrubio@MacBook-Pro.lan>
2024-04-13 15:20:20 -0700

    auth token works, also user is now being displayed on page when submitting info

--- React-Forms/src/components/Authenticate.jsx
+++ React-Forms/src/components/Authenticate.jsx
@@ -1,8 +1,44 @@
+import { useState } from "react";
 
-export default function Authenticate () {
-    return (
-        <>
-        <h2>Authenticate</h2>
-        </>
-    )
+export default function Authenticate({ token }) {
+  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [username, setUsername] = useState("");
+
+  async function handleClick() {
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/authenticate",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const result = await response.json();
+      console.log("result", result);
+      setSuccessMessage(result.message);
+
+      setUsername(result.data.username)
+
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Authenticate</h2>
+
+      {successMessage && <p>{successMessage}</p>}
+      {username && <p> Welcome  {username} ! </p>}
+
+      {error && <p>{error}</p>}
+
+      <button onClick={handleClick}> Authenticate Token!</button>
+    </div>
+  );
 }
--- React-Forms/src/components/SignUpForm.jsx
+++ React-Forms/src/components/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function SignUpForm() {
+export default function SignUpForm({ setToken }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
@@ -9,31 +9,37 @@ export default function SignUpForm() {
     e.preventDefault();
 
     try {
-        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
-                method: "POST",
-                body: JSON.stringify({username, password})
-                },
-        );
-        const result = await response.json(); 
-        console.log(result);
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
 
+      const result = await response.json();
+      console.log("result", result);
+      setToken(result.token);
     } catch (error) {
-        setError(error.message)
+      setError(error.message);
     }
-    //set" "
+    setUsername(""); // clears form to default value
+    setPassword("");
   }
 
   return (
     <>
       <h2>Sign Up </h2>
-      {error && <p>{error}</p>} {/* '&&' evaluates to 'true' if both are true, it evaluates to false. The double && is being used for conditional rendering: it renders the <p> element only if error is truthy*/}
-
+      {error && <p>{error}</p>}{" "}
+      {/* The double && is being used for conditional rendering: it renders the <p> element only if error is truthy*/}
       <form onSubmit={handleSubmit}>
-
         <label>
           Username:{" "}
           <input
-            value={username}
+            value={username} // updates as state updates
             onChange={(e) => setUsername(e.target.value)}
           />
         </label>
@@ -43,7 +49,7 @@ export default function SignUpForm() {
           Password:{" "}
           <input
             type="password"
-            value={password}
+            value={password} // updates as state updates
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
